70e7667ee2ae020cf830100dd302f594bd95ea6e
Jean Caillé <jcaille@gmail.com>
2013-10-22 22:31:42 +0200

    Added State management

--- Savr/SAVR_FluxLoader.m
+++ Savr/SAVR_FluxLoader.m
@@ -20,9 +20,6 @@
     return NO;
 }
 
--(void) cleanDirectory
-{
-}
 
 #pragma mark - FILE MANAGEMENT
 
@@ -37,4 +34,74 @@
     return folder;
 }
 
+-(void) cleanDirectory
+{
+    NSString* fluxDirectory = [self getOrCreateFluxDirectory];
+    NSArray* content = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:fluxDirectory error:nil];
+    for(NSString* item in content)
+    {
+        NSString* itemFullPath = [fluxDirectory stringByAppendingPathComponent:item];
+        [[NSFileManager defaultManager] removeItemAtPath:itemFullPath error:nil];
+    }
+}
+
+#pragma mark - STATE MANAEGEMENT
+
+-(BOOL) isActive
+{
+    NSString* applicationSupportDirectory = [SAVR_Utils getOrCreateDocumentDirectory];
+    NSArray* content = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:applicationSupportDirectory error:nil];
+    for(NSString* item in content){
+        if([item isEqualToString:fluxName]){
+            //Check if folder is symlink
+            NSString* itemFullPath = [applicationSupportDirectory stringByAppendingPathComponent:item];
+            NSString* itemType = [[[NSFileManager defaultManager]
+                                   attributesOfItemAtPath:itemFullPath error:nil]
+                                  fileType];
+            if([itemType isEqualToString:NSFileTypeSymbolicLink])
+            {
+                return YES;
+            }
+        }
+    }
+    return NO;
+
+}
+
+-(BOOL)setFluxAsActive
+{
+    if ([self isActive]) {
+        return YES;
+    }
+    NSString* fluxDirectory = [self getOrCreateFluxDirectory];
+    NSString* applicationSupportDirectory = [[SAVR_Utils getOrCreateDocumentDirectory] stringByAppendingPathComponent:fluxName];
+    NSError* error;
+    if(![[NSFileManager defaultManager] createSymbolicLinkAtPath:applicationSupportDirectory withDestinationPath:fluxDirectory error:&error])
+    {
+        return NO;
+    }
+    return YES;
+}
+
+-(BOOL)setFluxasInactive
+{
+    NSString* applicationSupportDirectory = [SAVR_Utils getOrCreateDocumentDirectory];
+    NSArray* content = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:applicationSupportDirectory error:nil];
+    for(NSString* item in content){
+        if([item isEqualToString:fluxName]){
+            //Check if folder is symlink
+            NSString* itemFullPath = [applicationSupportDirectory stringByAppendingPathComponent:item];
+            NSString* itemType = [[[NSFileManager defaultManager]
+                                  attributesOfItemAtPath:itemFullPath error:nil]
+                                  fileType];
+            if([itemType isEqualToString:NSFileTypeSymbolicLink])
+            {
+                //Remove item
+                [[NSFileManager defaultManager] removeItemAtPath:itemFullPath error:nil];
+            }
+        }
+    }
+    //Returns yes even if flux was not initially active
+    return YES;
+}
 @end
