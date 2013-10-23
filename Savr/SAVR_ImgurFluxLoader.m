adbe6d2f6c67ddf0da8f1deddfb58701fa29cc60
Jean Caillé <jcaille@gmail.com>
2013-10-23 21:10:25 +0200

    Basic filtering and fetching

--- Savr/SAVR_ImgurFluxLoader.m
+++ Savr/SAVR_ImgurFluxLoader.m
@@ -31,7 +31,9 @@
     return self;
 }
 
--(NSDictionary*) fetchSubredditImageList
+#pragma mark - FETCH GLOBAL INFORMATION
+
+-(NSArray*) fetchSubredditImageList
 {
     // Construct URL https://api.imgur.com/3/r/subreddit_name/top/week/.jsson
     NSString* completeUrl = [[[[@"https://api.imgur.com/3/gallery"
@@ -50,17 +52,52 @@
     }] asJson];
     
     if(response.code == 200){
-        return response.body.object;
+        NSDictionary* object = response.body.object;
+        NSArray* data = [object objectForKey:@"data"];
+        return data;
     } else {
-        NSLog(@"Erreur %i", response.code);
-        return @{};
+        NSLog(@"Error code : %d", (int)response.code);
+        return @[];
     }
+}
 
+#pragma mark - FILTER IMAGES
+
+
+-(BOOL) isImageHighRes:(NSDictionary*) image
+{
+    NSNumber* width = [image objectForKey:@"width"];
+    NSNumber* height = [image objectForKey:@"height"];
+    return (MIN(width, height) > [NSNumber numberWithInt:1000]);
+}
+
+-(BOOL) isImageAcceptable:(NSDictionary*) image
+{
+    return  [self isImageHighRes:image];
+}
+
+#pragma mark - FETCH SINGLE IMAGE
+-(BOOL) fetchSingleImage:(NSDictionary*) image
+{
+    NSString* imageURL = [image objectForKey:@"link"];
+    NSString* imageName = [imageURL lastPathComponent];
+    NSLog(@"Fetching %@", imageName);
+    NSData* imageData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: imageURL]];
+    NSString* imagePath = [[self getOrCreateFluxDirectory] stringByAppendingPathComponent:imageName];
+    [imageData writeToFile:imagePath atomically:YES];
+    return YES;
 }
 
 -(BOOL) fetch
 {
-    [self fetchSubredditImageList];
+    NSLog(@"Getting image list");
+    NSArray* images = [self fetchSubredditImageList];
+    NSLog(@"got iamage lists");
+    for(NSDictionary* image in images){
+        if([self isImageAcceptable:image]){
+            [self fetchSingleImage:image];
+        }
+    }
     return YES;
 }
 
