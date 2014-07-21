7f746b9fe3073b894a8893596ee49943283860fa
Jean Caille <jean.caille@polytechnique.edu>
2014-07-20 18:51:07 -0700

    Create Savr_Secrets.h
    Dummy implementation of SavrSecrets

---
+++ Savr/Savr_Secrets.h
@@ -0,0 +1,15 @@
+//
+//  SAVR_Secrets.h
+//  Savr
+//
+//  Created by Jean Caillé on 21/10/13.
+//  Copyright (c) 2013 Jean Caillé. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAVR_Secrets : NSObject
+extern NSString *const imgurClientID;
+extern NSString *const imgurClientSecret;
+extern NSString *const imgurAuthHeader;
+@end
