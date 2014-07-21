8aa20535ed748b4fc24daffb21ddf03041a0a868
Jean Caille <jean.caille@polytechnique.edu>
2014-07-20 18:52:20 -0700

    Create Savr_Secrets.m
    Dummy secrets

---
+++ Savr/Savr_Secrets.m
@@ -0,0 +1,15 @@
+//
+//  SAVR_Secrets.m
+//  Savr
+//
+//  Created by Jean Caillé on 21/10/13.
+//  Copyright (c) 2013 Jean Caillé. All rights reserved.
+//
+
+#import "SAVR_Secrets.h"
+
+@implementation SAVR_Secrets
+NSString *const imgurClientID = @"YourClientID";
+NSString *const imgurClientSecret = @"YourClientSecret";
+NSString *const imgurAuthHeader = @"YourAuthHeader";
+@end
